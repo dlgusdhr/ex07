4cb2166021f5edd5aef65d94afc049089eb4fcf8
dlgusdhr <dlgusdhr0501@naver.com>
2023-04-15 11:41:33 +0900

    ver1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import './App.css';
 import RouterPage from './components/RouterPage';
 import any from './images/any.jpg'
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
         <img src={any} width="100%"/>
             <h1>리액트 시작</h1>
             <RouterPage/>
-    </div>
+    </Container>
   );
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import {Row, Col} from 'react-bootstrap'
-
+import { Row, Col } from 'react-bootstrap'
 const HomePage = () => {
-  return (
-    <div>
-        <Row > 
+    return (
+
+        <Row className='my-5'>
             <Col>
                 <h1 className='text-center'>홈</h1>
-
             </Col>
         </Row>
 
-    </div>
-  )
+    )
 }
 
 export default HomePage
